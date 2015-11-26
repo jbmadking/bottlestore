9be1fe33d6a34a6691306d689bb5f3b09fe88cde
jbmadking <jbmatikinye@gmail.com>
2015-11-26 21:52:18 +0200

    - Getting closer to a successful build --debug

--- tests/acceptance/AddNewCategoryCept.php
+++ tests/acceptance/AddNewCategoryCept.php
@@ -1,35 +1,21 @@
 <?php
+$I = new AcceptanceTester($scenario);
+
+$I->am('Site Administrator');
+$I->wantTo('Add a new Category');
+
+$I->amLoggedAs($I->aSiteAdministrator());
 
 
-var_dump('app' . env('APP_ENV', ''));
-var_dump('host' . env('DB_HOST', 'localhost'));
-var_dump('database' . env('DB_DATABASE', 'skills-app'));
-var_dump('username' . env('DB_USERNAME', 'root'));
-var_dump('password' . env('DB_PASSWORD', ''));
-die();
+$I->amOnPage('/admin/categories');
 
-//
-//$I = new AcceptanceTester($scenario);
-//
-//
-//
+$I->click('Add Category');
+$I->seeCurrentUrlEquals('/admin/categories/create');
 
-//
-//$I->am('Site Administrator');
-//$I->wantTo('Add a new Category');
-//
-//$I->amLoggedAs($I->aSiteAdministrator());
-//
-//
-//$I->amOnPage('/admin/categories');
-//
-//$I->click('Add Category');
-//$I->seeCurrentUrlEquals('/admin/categories/create');
-//
-//$I->fillField('name', 'Malt Liquor');
-//$I->fillField('description', 'Malt Liquor for you to describe');
-//
-//$I->click('Create Category');
-//$I->seeCurrentUrlEquals('/admin/categories');
-//
-//$I->see('New Category: Malt Liquor Created');
+$I->fillField('name', 'Malt Liquor');
+$I->fillField('description', 'Malt Liquor for you to describe');
+
+$I->click('Create Category');
+$I->seeCurrentUrlEquals('/admin/categories');
+
+$I->see('New Category: Malt Liquor Created');
